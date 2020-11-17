088a855e2642e9fb20de37e4a623af6ba63be11f
David Hernandez <davidhernandezruiz@gmail.com>
2020-11-17 10:20:17 +0100

    Fixes backend url

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,6 +1,6 @@
 export default {
   search(query) {
-    let url = 'http://api.ftm.coleseguro.es:3000/search?'
+    let url = 'https://api.ftm.coleseguro.es:3000/search?'
 
     for (const parameter in query) {
       const value = query[parameter]
