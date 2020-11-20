da72250bf4c883fb564b6095651eb33edb82eb51
David Hernandez <davidhernandezruiz@gmail.com>
2020-11-20 15:22:10 +0100

    Adds some cleanup to the csv file generation.

--- api/src/controllers/download.js
+++ api/src/controllers/download.js
@@ -15,10 +15,22 @@ export default function download(req, res) {
       hits.map(hit => {
         const data = hit['_source']
         const { id, link, title, updated, contractType, contractSubtype, status, budget, processingType, contractingAuthority } = data
-        csv += `"${id}", "${link}", "${title}", "${updated}", "${contractType}", "${contractSubtype}", "${status}", "${budget}", "${processingType}", "${contractingAuthority}"\r\n`
+        csv += `"${cleanup(id)}","${cleanup(link)}","${cleanup(title)}","${cleanup(updated)}",`
+          + `"${cleanup(contractType)}","${cleanup(contractSubtype)}","${cleanup(status)}",`
+          + `"${cleanup(budget)}","${cleanup(processingType)}","${cleanup(contractingAuthority)}"\r\n`
       })
 
       res.set('Content-Type', 'text/csv');
       res.send(csv)
     })
+}
+
+function cleanup(string) {
+  string = string.split("\r").join('')
+  string = string.split("\n").join('')
+  string = string.split("\t").join('')
+  string = string.split(`"`).join('\'')
+  string = string.split(`“`).join('\'')
+  string = string.split(`”`).join('\'')
+  return string
 }
